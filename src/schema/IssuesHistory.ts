35bdc320b9dacebfe99ecedbd760347758983ab7
Duncan Leo <hello@duncanleo.me>
2025-03-08 23:29:11 +0800

    feat: rework issues history to display sectioned results

--- src/schema/IssuesHistory.ts
+++ src/schema/IssuesHistory.ts
@@ -1,14 +1,25 @@
 import { z } from 'zod';
-import { IssueSchema } from './Issue';
+import { IssueReferenceSchema } from './Overview';
+
+export const IssuesHistoryPageSectionSchema = z.object({
+  id: z.string(),
+  sectionStartAt: z.string().date(),
+  sectionEndAt: z.string().date(),
+  issueRefs: z.array(IssueReferenceSchema),
+});
+export type IssuesHistoryPageSection = z.infer<
+  typeof IssuesHistoryPageSectionSchema
+>;
+
+export const IssuesHistoryPageSchema = z.object({
+  startAt: z.string().date(),
+  endAt: z.string().date(),
+  sections: z.array(IssuesHistoryPageSectionSchema),
+});
+export type IssuesHistoryPage = z.infer<typeof IssuesHistoryPageSchema>;
 
 export const IssuesHistorySchema = z.object({
   pageCount: z.number(),
   fileNames: z.array(z.string()),
 });
 export type IssuesHistory = z.infer<typeof IssuesHistorySchema>;
-
-export const IssuesHistoryPageSchema = z.object({
-  pageNo: z.number(),
-  issues: z.array(IssueSchema),
-});
-export type IssuesHistoryPage = z.infer<typeof IssuesHistoryPageSchema>;
--- src/scripts/generateProducts/buildIssuesHistory.ts
+++ src/scripts/generateProducts/buildIssuesHistory.ts
@@ -5,7 +5,10 @@ import { IssueModel } from '../../model/IssueModel';
 import type {
   IssuesHistory,
   IssuesHistoryPage,
+  IssuesHistoryPageSection,
 } from '../../schema/IssuesHistory';
+import type { Issue } from '../../schema/Issue';
+import { assert } from '../../util/assert';
 
 const PAGE_SIZE = 10;
 
@@ -27,17 +30,33 @@ export function buildIssuesHistory() {
     const diffSeconds = startAtA.diff(startAtB).as('seconds');
 
     if (diffSeconds < 0) {
-      return 1;
+      return -1;
     }
     if (diffSeconds > 0) {
-      return -1;
+      return 1;
     }
     return 0;
   });
 
-  for (let i = 0; i < issues.length; i += PAGE_SIZE) {
+  type QuarterIsoDate = string;
+  type SectionIsoDate = string;
+  const pagesMap: Record<QuarterIsoDate, Record<SectionIsoDate, Issue[]>> = {};
+
+  for (const issue of issues) {
+    const startAt = DateTime.fromISO(issue.startAt);
+    const quarter = startAt.startOf('quarter').toISODate();
+    assert(quarter != null);
+    const month = startAt.startOf('month').toISODate();
+    assert(month != null);
+    const quarterIssues = pagesMap[quarter] ?? {};
+    const sectionIssues = quarterIssues[month] ?? [];
+    sectionIssues.push(issue);
+    quarterIssues[month] = sectionIssues;
+    pagesMap[quarter] = quarterIssues;
+  }
+
+  for (const [quarterIsoDate, sectionMap] of Object.entries(pagesMap)) {
     content.pageCount++;
-    const chunk = issues.slice(i, i + PAGE_SIZE);
     const chunkFileName = `issues_history_page_${content.pageCount}.json`;
     const chunkFilePath = join(
       import.meta.dirname,
@@ -45,9 +64,40 @@ export function buildIssuesHistory() {
       chunkFileName,
     );
     content.fileNames.push(chunkFileName);
+    const quarterEndAt = DateTime.fromISO(quarterIsoDate)
+      .endOf('quarter')
+      .toISODate();
+    assert(quarterEndAt != null);
+
+    const sections: IssuesHistoryPageSection[] = [];
+    // Reverse to ensure reverse-chronological order
+    for (const [sectionIsoDate, issues] of Object.entries(
+      sectionMap,
+    ).reverse()) {
+      const sectionStartAt = DateTime.fromISO(sectionIsoDate)
+        .startOf('month')
+        .toISO();
+      assert(sectionStartAt != null);
+      const sectionEndAt = DateTime.fromISO(sectionIsoDate)
+        .endOf('month')
+        .toISO();
+      assert(sectionEndAt != null);
+      const section: IssuesHistoryPageSection = {
+        id: sectionIsoDate,
+        sectionStartAt,
+        sectionEndAt,
+        // Reverse to ensure reverse-chronological order
+        issueRefs: issues
+          .reverse()
+          .map(({ updates, ...otherProps }) => otherProps),
+      };
+      sections.push(section);
+    }
+
     const page: IssuesHistoryPage = {
-      pageNo: content.pageCount,
-      issues: chunk,
+      startAt: quarterIsoDate,
+      endAt: quarterEndAt,
+      sections,
     };
     writeFileSync(chunkFilePath, JSON.stringify(page, null, 2));
   }
