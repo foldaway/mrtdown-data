470c6a6024a7341881a1b721bf4200311c8e50a3
Duncan Leo <hello@duncanleo.me>
2025-03-19 22:26:00 +0800

    fix(product): exclude overlapping time between issues of the same type

--- src/scripts/generateProducts/buildStatistics.ts
+++ src/scripts/generateProducts/buildStatistics.ts
@@ -1,10 +1,18 @@
 import { writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { IssueModel } from '../../model/IssueModel';
-import { DateTime } from 'luxon';
+import { DateTime, Duration } from 'luxon';
 import { assert } from '../../util/assert';
 import type { Statistics } from '../../schema/Statistics';
 import { ComponentModel } from '../../model/ComponentModel';
+import type { IssueReference } from '../../schema/Overview';
+import type { IssueType } from '../../schema/Issue';
+import type { DateSummary } from '../../schema/DateSummary';
+
+interface DateSummaryPartial {
+  issues: IssueReference[];
+  issueTypesMinutesOfDayMap: Record<IssueType, Record<number, boolean>>;
+}
 
 export function buildStatistics() {
   const issues = IssueModel.getAll();
@@ -26,6 +34,9 @@ export function buildStatistics() {
     import.meta.dirname,
     '../../../data/product/statistics.json',
   );
+
+  // This calculation excludes overlapping time between two issues of the same type (e.g. two overlapping disruptions)
+  const datesPartial: Record<string, DateSummaryPartial> = {};
 
   const content: Statistics = {
     dates: {},
@@ -74,7 +85,9 @@ export function buildStatistics() {
     }
 
     const startAt = DateTime.fromISO(issue.startAt).setZone('Asia/Singapore');
+    assert(startAt.isValid);
     const endAt = DateTime.fromISO(issue.endAt).setZone('Asia/Singapore');
+    assert(endAt.isValid);
 
     const dayCount = endAt.diff(startAt).as('days');
 
@@ -93,16 +106,23 @@ export function buildStatistics() {
     for (let i = 0; i < dayCount; i++) {
       const segmentStart = startAt.plus({ days: i });
       const segmentEnd = DateTime.min(endAt, segmentStart.plus({ days: 1 }));
-      const durationMs = segmentEnd.diff(segmentStart).as('milliseconds');
+      const dayStart = segmentStart.startOf('day');
       const segmentStartIsoDate = segmentStart.toISODate();
-      assert(segmentStartIsoDate != null);
-      const dateSummary = content.dates[segmentStartIsoDate] ?? {
-        issueTypesDurationMs: {},
+      const dateSummary = datesPartial[segmentStartIsoDate] ?? {
+        issueTypesMinutesOfDayMap: {},
         issues: [],
       };
-      let issueTypeDuration = dateSummary.issueTypesDurationMs[issue.type] ?? 0;
-      issueTypeDuration += durationMs;
-      dateSummary.issueTypesDurationMs[issue.type] = issueTypeDuration;
+
+      const issueTypeMinutesOfDay =
+        dateSummary.issueTypesMinutesOfDayMap[issue.type] ?? {};
+      for (
+        let j = segmentStart.diff(dayStart).as('minutes');
+        j < segmentEnd.diff(dayStart).as('minutes');
+        j++
+      ) {
+        issueTypeMinutesOfDay[j] = true;
+      }
+      dateSummary.issueTypesMinutesOfDayMap[issue.type] = issueTypeMinutesOfDay;
       dateSummary.issues.push({
         id: issue.id,
         type: issue.type,
@@ -111,8 +131,26 @@ export function buildStatistics() {
         startAt: issue.startAt,
         endAt: issue.endAt,
       });
-      content.dates[segmentStartIsoDate] = dateSummary;
+      datesPartial[segmentStartIsoDate] = dateSummary;
     }
+  }
+
+  for (const [dateIso, dateSummaryPartial] of Object.entries(datesPartial)) {
+    const issueTypesDurationMs: DateSummary['issueTypesDurationMs'] = {};
+
+    for (const [issueType, minutesOfDayMap] of Object.entries(
+      dateSummaryPartial.issueTypesMinutesOfDayMap,
+    )) {
+      issueTypesDurationMs[issueType as IssueType] = Duration.fromObject({
+        minutes: Object.values(minutesOfDayMap).filter((val) => val === true)
+          .length,
+      }).as('milliseconds');
+    }
+
+    content.dates[dateIso] = {
+      issues: dateSummaryPartial.issues,
+      issueTypesDurationMs,
+    };
   }
 
   writeFileSync(filePath, JSON.stringify(content, null, 2));
