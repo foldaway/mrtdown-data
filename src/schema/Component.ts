cdb0cc4f81e3f82533eece19fa9b74767528a949
Duncan Leo <hello@duncanleo.me>
2025-06-13 20:08:22 +0800

    feat(data): introduce `Component.type` field

--- src/schema/Component.ts
+++ src/schema/Component.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 import { StationIdSchema } from './StationId';
 
+export const ComponentTypeSchema = z.enum(['mrt.high', 'mrt.medium', 'lrt']);
+export type ComponentType = z.infer<typeof ComponentTypeSchema>;
+
 export const ComponentIdSchema = z
   .string()
   .refine((val) => /^[A-Z]{3,10}$/.test(val));
@@ -20,6 +23,7 @@ export const ComponentSchema = z.object({
   id: ComponentIdSchema,
   title: z.string(),
   title_translations: z.record(z.string(), z.string()),
+  type: ComponentTypeSchema,
   color: z.string().refine((val) => /^#([A-Fa-f0-9]{6})/.test(val)),
   startedAt: z.string().date(),
   branches: z.record(z.string(), ComponentBranchSchema),
