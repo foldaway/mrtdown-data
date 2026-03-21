46449119ce592ad72c4a64e641b3cb7e86c72c18
Duncan Leo <hello@duncanleo.me>
2026-03-21 18:51:02 +0800

    fix: export all schemas

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,27 @@
 export { FileStore } from '#repo/common/FileStore.js';
 export { MRTDownRepository } from '#repo/MRTDownRepository.js';
-export {
-  type IssueBundle,
-  IssueBundleSchema,
-} from '#schema/issue/bundle.js';
-export {
-  type CauseDisruption,
-  CauseDisruptionSchema,
-  type CauseInfra,
-  CauseInfraSchema,
-  type CauseMaintenance,
-  CauseMaintenanceSchema,
-  type CauseSubtype,
-  CauseSubtypeSchema,
-} from '#schema/issue/cause.js';
-export {
-  type AffectedEntity,
-  type AffectedEntityFacility,
-  AffectedEntityFacilitySchema,
-  AffectedEntitySchema,
-  type AffectedEntityService,
-  AffectedEntityServiceSchema,
-  type EntityImpactState,
-  EntityImpactStateSchema,
-} from '#schema/issue/entity.js';
-export {
-  type Evidence,
-  EvidenceSchema,
-} from '#schema/issue/evidence.js';
-export {
-  type FacilityEffect,
-  FacilityEffectSchema,
-} from '#schema/issue/facilityEffect.js';
-export {
-  type ImpactEvent,
-  ImpactEventSchema,
-} from '#schema/issue/impactEvent.js';
-export { type Issue, IssueSchema } from '#schema/issue/issue.js';
-export {
-  type IssueType,
-  IssueTypeSchema,
-} from '#schema/issue/issueType.js';
-export {
-  type Period,
-  PeriodSchema,
-} from '#schema/issue/period.js';
-export {
-  type ServiceEffect,
-  ServiceEffectSchema,
-} from '#schema/issue/serviceEffect.js';
-export {
-  type ServiceScope,
-  ServiceScopeSchema,
-} from '#schema/issue/serviceScope.js';
+
+export * from '#schema/common.js';
+export * from '#schema/issue/bundle.js';
+export * from '#schema/issue/cause.js';
+export * from '#schema/issue/claim.js';
+export * from '#schema/issue/entity.js';
+export * from '#schema/issue/evidence.js';
+export * from '#schema/issue/facilityEffect.js';
+export * from '#schema/issue/id.js';
+export * from '#schema/issue/impactEvent.js';
+export * from '#schema/issue/issue.js';
+export * from '#schema/issue/issueType.js';
+export * from '#schema/issue/period.js';
+export * from '#schema/issue/serviceEffect.js';
+export * from '#schema/issue/serviceScope.js';
+export * from '#schema/Landmark.js';
+export * from '#schema/Line.js';
+export * from '#schema/Operator.js';
+export * from '#schema/Service.js';
+export * from '#schema/Station.js';
+export * from '#schema/Town.js';
+
 export { FileWriteStore } from '#write/common/FileWriteStore.js';
 export { IdGenerator } from '#write/id/IdGenerator.js';
 export { MRTDownWriter } from '#write/MRTDownWriter.js';
